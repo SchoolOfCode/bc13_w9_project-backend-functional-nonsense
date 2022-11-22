3a3eeadcae4d4a3b4518955f0905e630d882db90
Seb French <Seb@Sebs-MacBook-Pro.local>
2022-11-22 12:29:42 +0000

    users models/router complete

--- models/usersModel.js
+++ models/usersModel.js
@@ -1,8 +1,35 @@
 const { query } = require("../db/db.js");
 
+async function getUsers() {
+  // Query the database and return all userSelect: 
+  const result = await query("SELECT * FROM users");
+  return result.rows;
+}
 
+// async function getUserById(id) {
+//   // Query the database and return the user with a matching id
+//   const result = await query('SELECT * FROM users WHERE id = $1', [id]);
+//   const user = result.rows[0];
+//   return user;
+// }
 
+async function createUser(user) {
+  // Query the database to create an user and return the newly created user
+  const result = await query('INSERT INTO users (learning_style, mb_personality, religion, cultural_background, gender_id, sexuality, hobby) VALUES ($1, $2, $3, $4, $5, $6, $7)' [user[0]] [user[1]] [user[2]] [user[3]] [user[4]] [user[5]] [user[6]]);
+  const newUser = result.rows[0];
+  return newUser;
+}
+
+async function deleteUserById(id) {
+  // Query the database to delete an user and return the deleted user
+  const result = await query('DELETE FROM users WHERE id = $1' [id]);
+  const user = result.rows[0];
+  return user;
+}
 
 module.exports = {
-
+  getUsers,
+  // getUserById,
+  createUser,
+  deleteUserById,
 };
--- routes/usersRouter.js
+++ routes/usersRouter.js
@@ -3,9 +3,36 @@ const router = express.Router();
 const db = require('../db/db.js');
 
 const {
-
+  getUsers,
+  // getUserById,
+  createUser,
+  deleteUserById,
 } = require("../models/usersModel.js");
 
+router.get("/", async function (req, res) {
+  if (req.query.name !== undefined) {
+    const searchedUsers = await searchUserByName(req.query.name);
+    return res.json({ success: true, payload: searchedUsers });
+  }
 
+  const users = await getUsers();
+  res.json({ success: true, payload: users });
+});
+
+// router.get("/:id", async function (req, res) {
+//   const user = await getUserById(req.params.id);
+//   res.json({ success: true, payload: user });
+// });
+
+router.post("/", async function (req, res) {
+  const data = req.body;
+  const newUser = await createUser(data);
+  res.json({ success: true, payload: newUser });
+});
+
+router.delete("/:id", async function (req, res) {
+  const deletedUser = await deleteUserById(req.params.id);
+  res.json({ success: true, payload: deletedUser });
+});
 
 module.exports = router;
