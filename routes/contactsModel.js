ce36bfa9a590f3ac3cdb755239ea727c4e9e57d8
Seb French <Seb@Sebs-MacBook-Pro.local>
2022-11-24 10:12:29 +0000

    backend working

--- routes/contactsModel.js
+++ routes/contactsModel.js
@@ -1,12 +1,14 @@
 const { query } = require("../db/db.js");
 
+
 async function getContacts() {
     const retrieveContacts = await query ("SELECT * FROM contacts");
     return retrieveContacts.rows;
 }
 
-async function createNewContact() {
-    const createContact = await query ('INSERT INTO contacts (name, twitter, linked_in, facebook, slack, email) VALUES ($1, $2, $3, $4, $5, $6)');
+
+async function createNewContact(data) {
+    const createContact = await query ('INSERT INTO contacts (name, twitter, linked_in, facebook, slack, email) VALUES ($1, $2, $3, $4, $5, $6)', [data.name, data.twitter, data.linked_in, data.facebook, data.slack, data.email]);
     const newContact = createContact.rows[0];
     return newContact;
 }
--- routes/contactsRouter.js
+++ routes/contactsRouter.js
@@ -5,21 +5,21 @@ const db = require('../db/db.js');
 const {
  getContacts,
  createNewContact
-} = require("../models/contactsModel.js");
+} = require("./contactsModel.js");
 
 
 // Get route handler for contact by ID
 // && allContacts if no id is provided
 router.get("/", async function (req, res) {
-        const allContacts = await getContacts();
-        res.json(allContacts);
+    const contacts = await getContacts();
+    res.json({ success: true, payload: contacts });
 });
 
 // Create Contacts
 router.post("/", async function (req, res) {
     const data = req.body;
     const newContact = await createNewContact(data);
-    res.json({ success: ture, payload: newContact})
+    res.json({ success: true, payload: newContact})
 });
 
 
