7c3cd1ab487cffbbde7c08595eeec36b8b4d0390
Fliss317 <felicity.cullen@mail.com>
2022-11-24 10:06:06 +0000

    contacts router corrections

--- routes/contactsRouter.js
+++ routes/contactsRouter.js
@@ -11,15 +11,15 @@ const {
 // Get route handler for contact by ID
 // && allContacts if no id is provided
 router.get("/", async function (req, res) {
-        const allContacts = await getContacts();
-        res.json(allContacts);
+        const contacts = await getContacts();
+        res.json({ success: true, payload: contacts});
 });
 
 // Create Contacts
 router.post("/", async function (req, res) {
     const data = req.body;
     const newContact = await createNewContact(data);
-    res.json({ success: ture, payload: newContact})
+    res.json({ success: true, payload: newContact})
 });
 
 
